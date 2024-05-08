f61fbe92f8d4bcf099617748d4eabc26d6583e9a
Stefania <cardonastefania200@gmail.com>
2024-05-08 16:07:03 -0500

    "Encapsulamiento - Implementación de Métodos para Manipulación de Propiedades por [Stefania Delgado Cardona]"

--- Persona1.java
+++ Persona1.java
@@ -3,4 +3,30 @@ public class Persona1{
     private int Edad;
     private String FechaDeNacimiento;
 
+    public void establecerNombre(String Nombre){
+        this.Nombre = Nombre;
+    }
+
+    public String obtenerNombre(){
+        return this.Nombre;
+    }
+
+    public void establecerEdad(int Edad){ 
+        this.Edad = Edad;    
+    }
+
+    public int obtenerEdad(){
+        return this.Edad;
+    }
+
+    public void establecerFechaDeNacimiento(String FechaDeNacimiento){
+        this.FechaDeNacimiento = FechaDeNacimiento;
+    }
+
+    public String obtenerFechaDeNacimiento(){
+        return this.FechaDeNacimiento;
+    }
+
+    
+    
 }
