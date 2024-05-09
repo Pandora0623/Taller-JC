4c50a768d850de5990c8933e1ad73b836266a85e
Stefania <cardonastefania200@gmail.com>
2024-05-08 22:21:15 -0500

    "Herencia y Polimorfismo - Implementación del Método calcularSalario() en Clase Vendedor por [Stefania Delgado Cardona]"

--- Vendedor.java
+++ Vendedor.java
@@ -1,3 +1,43 @@
 public class Vendedor extends Empleado {
+
+    private float Ventas;
+    private float Horas;
+    private float v_Horas;
+
+    public Vendedor(String Nombre, float Salario, float Ventas, float Horas, float v_Horas){
+        super(Nombre,Salario);
+        this.Ventas = Ventas;
+        this.Horas = Horas;
+        this.v_Horas = v_Horas;
+    }
+
+    public float getVentas(){
+        return Ventas;
+    }
+
+    public void setVentas(float Ventas){
+        this.Ventas = Ventas;
+    }
+
+    public float getHoras(){
+        return Horas;
+    }
+
+    public void setHoras(float Horas){
+        this.Horas = Horas;
+    }
+
+    public float getv_Horas(){
+        return v_Horas;
+    }
+
+    public void setv_Horas(float v_Horas){
+        this.v_Horas = v_Horas;
+    }
+
+    public float CalcularSalario(){
+        return (v_Horas*Horas)+(Ventas*(10/100));
+    }
+
     
 }
