bc98e8be3c21036aa304c06d0518d05dd29ee7d4
Stefania <cardonastefania200@gmail.com>
2024-05-08 19:17:23 -0500

    "Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por [Stefania Delgado Cardona]"

--- FigurasGeometricas.java
+++ FigurasGeometricas.java
@@ -1,5 +1,22 @@
 public class FigurasGeometricas {
     private String Nombre;
     private String Color;
+
+    public void establecerNombre(String Nombre){
+        this.Nombre = Nombre;
+    }
+
+    public String obtenerNombre(){
+        return this.Nombre;
+    }
+
+    public void establecerColor(String Color){
+        this.Color = Color;
+    }
+
+    public String obtenerColor(){
+        return this.Color;
+    }
+
     
 }
